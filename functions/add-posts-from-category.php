03270738fbcbb8094d20d85fcd5f8744b073a6ef
travis <travis@sparxoo.com>
2018-05-14 17:52:12 -0400

    comments

--- functions/add-posts-from-category.php
+++ functions/add-posts-from-category.php
@@ -1,22 +1,21 @@
 <?php
 function posts_from_category($category_name) {
-// the query
-$the_query = new WP_Query( array( 'category_name' => $category_name, 'posts_per_page' => 10 ) );
-// The Loop
-if ( $the_query->have_posts() ) {
+  // the query
+  $the_query = new WP_Query( array( 'category_name' => $category_name, 'posts_per_page' => 10 ) );
+  // The Loop
+  if ( $the_query->have_posts() ) {
     while ( $the_query->have_posts() ) {
-        $the_query->the_post();
-            // if no featured image is found
-            echo $category_name;
-            echo '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .'</a></li>';
-
-            }
-    } else {
-      echo '<span>nothing found</span>';
+      $the_query->the_post();
+        // if no featured image is found
+        echo $category_name;
+        echo '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .'</a></li>';
     }
+  } else {
+    echo '<span>nothing found</span>';
+  }
 
-/* Restore original Post Data */
-wp_reset_postdata();
+  /* Restore original Post Data */
+  wp_reset_postdata();
 }
 // Add a shortcode
 add_shortcode('categoryposts', 'posts_from_category');
