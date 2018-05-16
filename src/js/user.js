6494f5cd336f21f55bc333feb8d8db5cb5310c28
travis <travis@sparxoo.com>
2018-05-16 15:36:57 -0400

    jquery animations

--- src/js/user.js
+++ src/js/user.js
@@ -1,15 +1,17 @@
 // click user to show user details
 $('.user').click(function () {
   var $this = $(this);
-  $('.user').removeClass('is-active');
+  $('.user').not(this).removeClass('is-active');
   $this.toggleClass('is-active');
+  $('.user').not(this).next('.user__details').slideUp(150);
+  $this.next('.user__details').slideToggle("fast");
 });
 
 // close user details
 $('.section.for-users').on('click', '.user__close, .user__details', function () {
+  $('.user__details').slideUp("fast");
   $('.user').removeClass('is-active');
-  console.log('click');
-})
+});
 
 // hide categories that contain nothing
 $('.category').has('.nothing').hide();
