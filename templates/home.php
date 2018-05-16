78a4374bc01eba0430c4d88027774cc58c24b7a7
travis <travis@sparxoo.com>
2018-05-16 15:36:28 -0400

    individual skills display

--- templates/home.php
+++ templates/home.php
@@ -5,45 +5,47 @@
 <div class="section for-users">
 <?php
 $users = get_users('blog_id=1&orderby=nicename');
-foreach ($users as $user) :
-  $user_id = $user->ID;
-  $user_name = esc_html( $user->display_name );
-  $skills = get_field('skills', 'user_'. $user_id );
-  $user_photo = get_field('photo', 'user_'. $user_id );
-?>
-    <div class="user" style="<?php if ($user_photo) : ?>background-image: url(<?php echo $user_photo['url']; ?>); <?php endif; ?>">
-      <div class="user__preview">
 
-      </div>
-      <div class="user__details">
+?>
+  <?php
+  $columns = 4;
+  $row_count = 0;
+  $column_width = 12 / $columns;
+  ?>
+  <div class="row">
+   <?php foreach ($users as $user) :
+      $user_id = $user->ID;
+      $user_name = esc_html( $user->display_name );
+      $user_photo = get_field('photo', 'user_'. $user_id );
+    ?>
+    <div class="user col-md-<?php echo $column_width; ?>">
+      <div class="user__inner" style="<?php if ($user_photo) : ?>background-image: url(<?php echo $user_photo['url']; ?>); <?php endif; ?>">
+      </div><!-- /.user -->
+    </div><!-- /.user.col-md-4 -->
+    <div class="user__details">
+      <div class="details__inner container">
         <div class="user__close"></div>
         <h2 class="user__name"><?php echo $user_name; ?></h2>
-        <div class="categories-wrapper">
-        <?php $categories = get_categories(); ?>
-        <?php foreach ( $categories as $category ) : ?>
-        <?php $category_name = $category->name; ?>
-        <?php $category_id = get_cat_ID($category_name); ?>
-        <ul class="category category--<?php echo strtolower($category_name); ?>">
-          <?php $parent = get_cat_name($category->parent); ?>
-          <?php if( $skills ) : ?>
-            <h3 class="category__name"><?php if ($category_name == 'Uncategorized') { echo 'Other'; } else { echo $category_name; } ?></h3>
-            <?php foreach( $skills as $post): ?>
-              <?php if (has_category($category_name, $post->ID)) : ?>
-                <?php setup_postdata( $post ); ?>
-                <li><?php the_title(); ?></li>
-                <?php wp_reset_postdata(); ?>
-              <?php else : ?>
-              <?php endif;//has_category ?>
-            <?php endforeach;//$skills as $post ?>
-          <?php endif;//$skills ?>
-        </ul><!-- /.category -->
-        <?php endforeach;//$categories as $category ?>
-        </div><!-- /.categories-wrapper -->
-      </div><!-- /.user__details -->
-    </div><!-- /.user -->
-<?php
-endforeach; //foreach $user
-?>
+        <div class="categories">
+        <?php include(module('skills', 'performance')); ?>
+        <?php include(module('skills', 'storytelling')); ?>
+        <?php include(module('skills', 'experience')); ?>
+        <?php include(module('skills', 'other')); ?>
+        </div><!-- /.categories -->
+      </div><!-- /.details__inner -->
+    </div><!-- /.user__details -->
+    <?php
+    $row_count++;
+    if($row_count % $columns == 0) :
+    ?>
+    </div>
+
+    <div class="row<?php if ($row_count % 2 == 0) { echo " even"; } ?>">
+    <?php endif; ?>
+  <?php endforeach;//users as user ?>
+
+
+    </div><!-- /.row -->
 
 </div><!-- /.section.for-users -->
 <?php get_footer(); ?>
