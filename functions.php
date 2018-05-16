8ef0c786d696d51c47217341e9059cea1c607b1b
travis <travis@sparxoo.com>
2018-05-16 15:37:15 -0400

    helper function for including partial. this allows variables to be passed

--- functions.php
+++ functions.php
@@ -5,6 +5,15 @@ function include_function($filename) {
   return require_once( dirname( __FILE__ ) . '/functions/' . $filename . '.php' );
 }
 
+// helper function to include files from the template-parts folder
+function module($part1, $part2 = '') {
+  $hyphen = '';
+
+  if ($part2 != '') { $hyphen = '-'; }
+
+  return locate_template('template-parts/' . $part1 . $hyphen . $part2 . '.php');
+}
+
 include_function('add-theme-support');
 
 include_function('add-styles');
